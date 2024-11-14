15ca981452f811a54876d3b74c382ec4ff782980
yoonc01 <143938662+yoonc01@users.noreply.github.com>
2024-11-14 21:02:52 +0900

    day1.py done
    update readme and made the day1.py

---
+++ Day1/day1.py
@@ -0,0 +1,7 @@
+# \n을 사용하여 입력을 출력 문자열 밑에서 받도록 하였음
+name = input("What's the name of the city you grew up in?\n")
+pet_name = input("What's your pet's name?\n")
+
+band_name = name + " " + pet_name
+
+print("Your band name could be " + band_name)
--- Day1/readme.md
+++ Day1/readme.md
@@ -6,3 +6,6 @@ https://appbrewery.github.io/python-day1-demo/
 
 # Description
 사는 도시와 펫 이름을 입력 받고 그 둘을 합쳐 결과를 보여줌
+
+질문을 물어보면 커서는 질문 아래에 생김
+`input`을 사용했을 때 커서는 `input` 문자열 바로 옆에 생기기에 이에 대한 처리가 필요
