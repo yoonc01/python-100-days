ab8b89d0a30f72374b932d21aa227b58d2058075
yoonc01 <143938662+yoonc01@users.noreply.github.com>
2024-11-28 21:43:16 +0900

    Day10 docstring 추가

--- Day10_func_return/project.py
+++ Day10_func_return/project.py
@@ -1,4 +1,9 @@
 def return_valid_input(statement, valid_values):
+    # 아래 문구는 docstring이라는 함수를 설명하는 문장
+    # IDLE에서 함수 위에 마우스를 hover하면 아래 내용이 설명으로 나옴
+    """
+    statement을 요청하고 유효한 값이 입력되지 않으면 statement를 재요청
+    """
     data = input(statement).lower()
     while(data not in valid_values):
         print("Wrong Input")
@@ -7,6 +12,9 @@ def return_valid_input(statement, valid_values):
     return data
     
 def cal(a, op, b):
+    """
+    연산자에 따라 계산을 수행하는 함수
+    """
     if (op == '+'):
         result = a + b
     elif op == "-":
